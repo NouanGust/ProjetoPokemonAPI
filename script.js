4366d553ca3a90b8d15f999a24d0009ef7035416
Nohan Gustavo <81246430+NouanGust@users.noreply.github.com>
2025-05-12 11:26:10 -0300

    arquivos aula 12.05

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 // Elementos HTML
-let $input = document.querySelector("#input")
-let $pokemon_imagem = document.querySelector("#imagem_pokemon")
-let $pokemon_nome = document.querySelector("#pokemon_nome")
-let $form = document.querySelector("#submit_form")
+let $input_form = document.getElementById("input_form")
+let $input = document.getElementById("input")
+let $pokemon_nome = document.getElementById("pokemon_nome")
+let $pokemon_id = document.getElementById("pokemon_id")
+let $pokemon_imagem = document.getElementById("imagem_pokemon")
 
-async function fetchPokemon(pokemon) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+
+// Função PegaPokemon -- Fetch
+async function PegaPokemon(pokemon){
+    const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    let response = await(fetch(URL))
 
     if(response.status === 200) {
-        let pokemon_obj = await response.json()
+        let pokemon_obj = await(response.json())
         console.log(pokemon_obj)
+        console.log(pokemon_obj.sprites.front_default)
         return pokemon_obj
     }
 }
 
-async function mostrarPokemon(pokemon) {
-    let pokemon_dados = await fetchPokemon(pokemon)
-    
-    if(pokemon_dados){
-        $pokemon_nome.innerText = pokemon_dados.name
+// Função MostraPokemon -- Coloca os elementos no HTML
+async function MostraPokemon(pokemon) {
+    let pokemon_infos = await PegaPokemon(pokemon)
+
+    if(pokemon_infos){
+        $pokemon_nome.innerText = pokemon_infos.name
+        $pokemon_id.innerText = pokemon_infos.id
+        $pokemon_imagem.src = pokemon_infos.sprites.front_default
     }
 }
 
-$form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    mostrarPokemon($input.value.toLowerCase())
 
+// EventListener
+$input_form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    MostraPokemon($input.value.toLowerCase())
 })
+
+
+
+
+
+
+
+
+
+
+async function PrintPokemon(pokemon){
+    let response = await(fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`))
+    let pokemon_obj = await response.json()
+    console.log(pokemon_obj)
+    console.log(pokemon_obj.id)
+    console.log(pokemon_obj.name)
+}
